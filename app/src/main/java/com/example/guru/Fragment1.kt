456e6d0e7fe71c411b87eabdd3a54799bf7f8f43
gogowonji <65698313+gogowonji@users.noreply.github.com>
2020-08-16 01:25:12 +0900

    Todolist ing....

--- app/src/main/java/com/example/guru/Fragment1.kt
+++ app/src/main/java/com/example/guru/Fragment1.kt
@@ -1,10 +1,16 @@
 package com.example.guru
 
+import android.graphics.Paint
+import android.graphics.Typeface
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.guru.databinding.Fragment1Binding
+import com.example.guru.databinding.TodoItemBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -16,26 +22,95 @@ private const val ARG_PARAM2 = "param2"
  * Use the [Fragment1.newInstance] factory method to
  * create an instance of this fragment.
  */
+
+
 class Fragment1 : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
 
+    //binding
+    private var _binding: Fragment1Binding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+
+    private val data = arrayListOf<Todo>()
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
+
         arguments?.let {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
         }
     }
 
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
+
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_1, container, false)
+
+        _binding = Fragment1Binding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+
+        data.add(Todo("work"))
+        data.add(Todo("hey",true))
+
+
+
+        binding.recyclerView.apply {
+            layoutManager = LinearLayoutManager(this@Fragment1.context)
+            adapter = TodoAdapter(data,
+                onClickDeleteIcon = {
+                    deleteTodo(it)
+                },
+                onClickItem = {
+                    toggleTodo(it)
+                }
+            )
+        }
+
+        binding.addButton.setOnClickListener(View.OnClickListener(){
+
+            fun onClick(v: View?) {
+                // Your Code Lines
+                addTodo()
+            }
+        })
     }
+
+
+    private fun toggleTodo(todo : Todo) {
+        todo.isDone = !todo.isDone
+        binding.recyclerView.adapter?.notifyDataSetChanged()
+    }
+
+    private fun addTodo() {
+        val todo = Todo(binding.editText.text.toString(), false)
+        data.add(todo)
+        binding.recyclerView.adapter?.notifyDataSetChanged()
+    }
+
+    private fun deleteTodo(todo: Todo) {
+        data.remove(todo)
+        binding.recyclerView.adapter?.notifyDataSetChanged()
+    }
+
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
 
     companion object {
         /**
@@ -55,5 +130,73 @@ class Fragment1 : Fragment() {
                     putString(ARG_PARAM2, param2)
                 }
             }
+
+
     }
 }
+
+data class Todo(
+    val text: String,
+    var isDone: Boolean = false
+)
+
+class TodoAdapter(
+    private val myDataset: List<Todo>,
+    val onClickDeleteIcon: (todo: Todo) -> Unit,
+    val onClickItem: (todo: Todo) -> Unit
+) :
+    RecyclerView.Adapter<TodoAdapter.TodoViewHolder>() {
+
+    // Provide a reference to the views for each data item
+    // Complex data items may need more than one view per item, and
+    // you provide access to all the views for a data item in a view holder.
+    // Each data item is just a string in this case that is shown in a TextView.
+    class TodoViewHolder(val binding: TodoItemBinding) : RecyclerView.ViewHolder(binding.root)
+
+    // Create new views (invoked by the layout manager)
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): TodoAdapter.TodoViewHolder {
+        // create a new view
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.todo_item, parent, false)
+        // set the view's size, margins, paddings and layout parameters
+
+
+        return TodoViewHolder(TodoItemBinding.bind(view))
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    override fun onBindViewHolder(holder: TodoViewHolder, position: Int) {
+        // - get element from your dataset at this position
+        // - replace the contents of the view with that element
+        val todo = myDataset[position]
+        holder.binding.TodoList.text = todo.text
+
+        if(todo.isDone){
+            holder.binding.TodoList.apply {
+                paintFlags  = paintFlags or Paint.STRIKE_THRU_TEXT_FLAG
+                setTypeface(null, Typeface.ITALIC)
+            }
+        }else{
+            holder.binding.TodoList.apply {
+                paintFlags = 0
+                setTypeface(null, Typeface.NORMAL)
+            }
+        }
+
+        holder.binding.delete.setOnClickListener {
+            onClickDeleteIcon.invoke(todo)
+        }
+
+        holder.binding.root.setOnClickListener{
+            onClickItem.invoke(todo)
+        }
+    }
+    // Return the size of your dataset (invoked by the layout manager)
+    override fun getItemCount() = myDataset.size
+}
+
+
+
